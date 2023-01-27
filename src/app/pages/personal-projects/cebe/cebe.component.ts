8afb9cabd4654817712ef3a4f2c67fae840fdcc6
Rafik D <rafik-d@MacBook-Air-de-Rafik.local>
2023-01-27 20:30:54 +0100

    cebe page done (opacity when scroll not yet)

--- src/app/pages/personal-projects/cebe/cebe.component.ts
+++ src/app/pages/personal-projects/cebe/cebe.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-cebe',
@@ -7,4 +7,46 @@ import { Component } from '@angular/core';
 })
 export class CebeComponent {
 
+  credit1: boolean = false;
+  credit2: boolean = false;
+  credit3: boolean = false;
+
+  reveal(credit: number) {
+    switch(credit) { 
+      case 1: { 
+        this.credit1 = true
+        break; 
+      } 
+      case 2: { 
+        this.credit2 = true
+        break; 
+      }
+      case 3: { 
+        this.credit3 = true
+        break; 
+     } 
+      default: { 
+         break; 
+      } 
+    }
+  }
+  hide(credit: number) {
+    switch(credit) { 
+      case 1: { 
+        this.credit1 = false
+        break; 
+      } 
+      case 2: { 
+        this.credit2 = false
+        break; 
+      }
+      case 3: { 
+        this.credit3 = false
+        break; 
+     } 
+      default: { 
+         break; 
+      } 
+    }
+  }
 }
