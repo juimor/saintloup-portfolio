af33b2e0c99b5f29ad89c7316933765cc5ff8262
Rafik D <rafik-d@MacBook-Air-de-Rafik.local>
2023-01-28 15:51:48 +0100

    cheiron page done (custom cursor + animation on carousel not yet)

--- src/app/pages/personal-projects/cheiron/cheiron.component.ts
+++ src/app/pages/personal-projects/cheiron/cheiron.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-cheiron',
   templateUrl: './cheiron.component.html',
   styleUrls: ['./cheiron.component.scss']
 })
-export class CheironComponent {
+export class CheironComponent implements AfterViewInit {
+  firstImgSelected: string = '/assets/pages/cheiron/9.1.jpeg';
+  secondImgSelected: string = '/assets/pages/cheiron/10.1.jpeg';
+  firstImgBool: boolean = false;
+  secondIndex: number = 0;
 
+  ngAfterViewInit() {
+   this.findAProperName();
+  }
+
+  nextImage(image: string) {
+    switch (image) {
+      case 'first':
+        this.firstImgBool ? this.firstImgBool = false : this.firstImgBool = true;
+        console.log(this.firstImgBool)
+        if (this.firstImgSelected.includes('9.1')) this.firstImgSelected = '/assets/pages/cheiron/9.2.jpeg';
+        else this.firstImgSelected = '/assets/pages/cheiron/9.1.jpeg'
+        break;
+      case 'second':
+        if (this.secondImgSelected.includes('10.1')) this.secondImgSelected = '/assets/pages/cheiron/10.2.jpeg';
+        else this.secondImgSelected = '/assets/pages/cheiron/10.1.jpeg'
+        break;
+      default:
+        break;
+    }
+  }
+
+  findAProperName() {
+    setTimeout(() => {
+      this.nextImage('first');
+      this.findAProperName();
+    }, 3000);
+  }
 }
--- src/app/pages/personal-projects/portraits/portraits.component.ts
+++ src/app/pages/personal-projects/portraits/portraits.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portraits',
   templateUrl: './portraits.component.html',
   styleUrls: ['./portraits.component.scss']
 })
-export class PortraitsComponent {
+export class PortraitsComponent implements OnInit {
+  carouselItems: string[]= [];
+  imageSelected: string = '';
+  index: number = 0;
+  imageCounter: number = 7;
 
+  ngOnInit() {
+    this.initCarouselItems();
+  }
+
+  initCarouselItems() {
+    for (let i = 1; i <= this.imageCounter; i++) {
+      this.carouselItems.push('/assets/pages/portraits/'+i+'.jpeg');      
+    }
+    this.imageSelected = this.carouselItems[this.index];
+  }
+
+  nextImage(next: boolean) {
+    if(next) {
+      if (this.index === this.imageCounter) {
+        this.index = 0;
+        this.imageSelected = this.carouselItems[this.index];
+      }
+      if (this.index < this.imageCounter) this.imageSelected = this.carouselItems[this.index++];
+    } else {
+      if (this.index === 0) {
+        console.log('=== 0')
+        console.log(this.index)
+        this.imageSelected = this.carouselItems[this.imageCounter-1];
+        this.index = this.imageCounter-1;
+      } 
+      if (this.index > 0) {
+        console.log('> 0')
+        console.log(this.index)
+        this.imageSelected = this.carouselItems[this.index--];
+      }
+    }
+  }
 }
